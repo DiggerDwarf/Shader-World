9374bb576e4ca747a0e03e48f53a540b3235891f
DiggerDwarf <135308092+DiggerDwarf@users.noreply.github.com>
2024-06-04 20:05:40 +0200

    init

---
+++ shaders/fragment.glsl
@@ -0,0 +1,147 @@
+
+/* Main function, uniforms & utils */
+#ifdef GL_ES
+    precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec3 u_camera_pos;
+
+#define PI_TWO			1.570796326794897
+#define PI				3.141592653589793
+#define TWO_PI			6.283185307179586
+
+float t = 0.0;
+
+/* Coordinate and unit utils */
+vec2 coord(in vec2 p) {
+    p = p / u_resolution.xy *2.0 -1.0;
+    if (u_resolution.x > u_resolution.y) {
+        p.x *= u_resolution.x / u_resolution.y;
+    } else {
+        p.y *= u_resolution.y / u_resolution.x;
+    }
+    return p;
+}
+#define FAR 50.0
+#define max_steps 200.0
+uniform float nb_reflections;
+
+
+vec4 power(vec4 v, float p) {
+    return vec4(
+        pow(v.x,p),
+        pow(v.y,p),
+        pow(v.z,p),
+        pow(v.w,p)
+    );
+}
+
+float sdBox( vec3 p, vec3 b ) {
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+float round(float x) {
+    return floor(x+0.5);
+}
+
+float sphere(vec3 p) { return length(p-vec3(-1.5,0,0))-1.0; }
+float box   (vec3 p) { return sdBox(p-vec3(1.5,0,0), vec3(0.6))-0.2;}
+float plane (vec3 p) { return p.y+1.0; }
+// float light (vec3 p) { return length(p-light_pos)-0.5; }
+
+float map(vec3 p) {
+    return min(min(
+        sphere(p),
+        plane(p)),
+        box(p)
+    );
+}
+
+#define normal_offset 0.001
+vec3 get_normal(vec3 pos) {
+    vec2 Dir = vec2(normal_offset,0);
+    return normalize(vec3(
+        map(pos+Dir.xyy)-map(pos-Dir.xyy),
+		map(pos+Dir.yxy)-map(pos-Dir.yxy),
+		map(pos+Dir.yyx)-map(pos-Dir.yyx)
+    ));
+}
+
+vec3 get_color(vec3 p) {
+    return vec3(
+        step(sphere(p),0.001*t)*vec3(0.1843, 0.451, 0.549) +
+        step(plane(p),0.001*t)*vec3(0.2627, 0.298, 0.1176) +
+        step(box(p),0.001*t)*vec3(0.5882, 0.1647, 0.1647)
+        // step(light(p),0.001*t)*light_color
+        //step(FAR-5., length(p))*vec3(0.851, 0.6863, 0.0235)
+    );
+}
+
+vec3 get_ambient(vec3 p, vec3 light_color, float strength) {
+    return get_color(p)*light_color*strength;
+}
+
+vec3 get_diffuse(vec3 p, vec3 l) {
+    vec3 normal = get_normal(p);
+    vec3 color = get_color(p);
+    return max(dot(normal,normalize(l-p)),0.0)*color;
+}
+
+vec3 get_specular(vec3 p, vec3 va, vec3 l, vec3 lc) {
+    vec3 normal = get_normal(p);
+    vec3 light_out = reflect((-l+p),normal);
+    vec3 color = get_color(p);
+    return pow(clamp(dot(-va,light_out)/20., 0.0, 1.0),8.)*lc;
+}
+
+
+void main() {
+    vec2 uv = coord(gl_FragCoord.xy);
+
+    vec3 ro = u_camera_pos;
+    // vec3 target = vec3(0.0, 0.0, 0.0);
+	// vec3 ww = normalize( target - ro );
+    // vec3 uu = normalize( cross(ww,vec3(0.0,1.0,0.0) ) );
+    // vec3 vv = normalize( cross(uu,ww));
+	// vec3 rd = normalize( uv.x*uu + uv.y*vv + ww );
+
+    vec3 rd = normalize(vec3(uv, 1));
+
+    t = 0.0;
+    float dist;
+    vec3 light_pos = vec3(1,2,1);
+    vec3 light_color = vec3(0.8157, 0.1686, 0.4588);
+
+    // vec3 light_pos = 10.*vec3(cos(u_time/3.),2,sin(u_time/3.));
+    // vec3 light_color = vec3(0.8235, 0.251, 0.5686);
+    float light_strength = 0.3;
+
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+
+
+    for(float reflection = 0.0; reflection < nb_reflections; reflection++) {
+        for (float steps = 0.0; steps < max_steps; steps++) {
+            dist = map(ro);
+            t += dist;
+            ro += dist*rd;
+
+            if (dist < 0.0001*t || t > FAR) break;
+        }
+
+        if (t > FAR) {
+            gl_FragColor += vec4(light_color*exp(-0.5*reflection), 1.0);
+            return;
+        }
+
+        gl_FragColor += power(vec4(
+            get_ambient(ro, light_color, light_strength) +
+            get_diffuse(ro, light_pos)*2.0 +
+            get_specular(ro, rd, light_pos, light_color)*0.3
+            ,1),1.5) * exp(-0.5*reflection);
+        
+        rd = reflect(rd, get_normal(ro));
+        ro += get_normal(ro)*0.01;
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+#define WIDTH  1280
+#define HEIGHT 720
+#define MOV_SPEED 5
+#define ROT_SPEED 10
+
+int reflections = 3;
+
+void Draw(sf::RenderWindow& window, sf::ConvexShape& quad, sf::Shader& shader, sf::Vector3f& pos, sf::Transform& rot) {
+    shader.setUniform("u_resolution", sf::Glsl::Vec2(window.getSize()));
+    shader.setUniform("u_camera_pos", sf::Glsl::Vec3(pos));
+    shader.setUniform("nb_reflections", float(reflections));
+    // shader.setUniform("camera_rot", sf::Glsl::Mat4(rot));
+    // shader.setUniform("u_color", sf::Glsl::Vec4(sf::Color(106, 61, 158)));
+    window.draw(quad, &shader);
+    window.display();
+}
+
+void Update(sf::RenderWindow& window, sf::Vector3f& pos, sf::Transform& rot, sf::Clock& clock) {
+
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::Closed:
+            window.close();
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
+
+    float dt = clock.restart().asSeconds();
+    std::cout << 1/dt << "\n";
+
+
+    /* Camera movement */ {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) { pos.z += MOV_SPEED * dt; }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { pos.z -= MOV_SPEED * dt; }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) { pos.x -= MOV_SPEED * dt; }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { pos.x += MOV_SPEED * dt; }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) { pos.y += MOV_SPEED * dt; }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)) { pos.y -= MOV_SPEED * dt; }
+    }
+
+    
+
+    // /* Camera rotation */ {
+    //     if (sf::Keyboard::isKeyPressed(sf::Keyboard::K)) { pos.z -= ROT_SPEED * dt; }
+    //     if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) { pos.z += ROT_SPEED * dt; }
+    //     if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) { pos.x -= ROT_SPEED * dt; }
+    //     if (sf::Keyboard::isKeyPressed(sf::Keyboard::L)) { pos.x += ROT_SPEED * dt; }
+    // }
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc >= 2) reflections = std::stoi(argv[1]);
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Shader World !", sf::Style::Close | sf::Style::Resize);
+    window.setFramerateLimit(60U);
+    sf::Clock clock;
+    sf::Shader shader;
+    if (!shader.loadFromFile("shaders/fragment.glsl", sf::Shader::Fragment)) {
+        return 1;
+    }
+    sf::ConvexShape quad(4);
+    quad.setPoint(0, sf::Vector2f(0,     0     ));
+    quad.setPoint(1, sf::Vector2f(WIDTH, 0     ));
+    quad.setPoint(2, sf::Vector2f(WIDTH, HEIGHT));
+    quad.setPoint(3, sf::Vector2f(0,     HEIGHT));
+
+    sf::Vector3f camera_pos(0, 0, -3);
+    sf::Transform camera_rot;
+
+    while (window.isOpen())
+    {
+        Draw(window, quad, shader, camera_pos, camera_rot);
+        Update(window, camera_pos, camera_rot, clock);
+    }
+}
